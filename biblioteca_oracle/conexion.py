b6860d6d8b01e1d58d40636f7e7d597ddb6ed5c8
xReNdo <xrendofps@gmail.com>
2025-12-03 23:36:21 -0300

    12-3-202555

--- biblioteca_oracle/conexion.py
+++ biblioteca_oracle/conexion.py
@@ -1,6 +1,7 @@
 import oracledb
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 username = os.getenv("ORACLE_USER")
@@ -8,12 +9,13 @@ dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
 
 with oracledb.connect(
-    user=username,
-    password=password,
-    dsn=dsn
-    ) as connection:
+        user=username,
+        password=password,
+        dsn=dsn
+) as connection:
     with connection.cursor() as cursor:
         sql = "select sysdate from dual"
         for row in cursor.execute(sql):
             for column in row:
                 print(column)
+
